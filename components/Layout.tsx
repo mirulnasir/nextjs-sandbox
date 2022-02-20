c1cbebb33c49090ac8d7493f7ab157c3056d4b43
mirulnasir <mirul@bestari.io>
2022-02-20 11:02:39 +1100

    added state-reducer page content

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,21 +6,25 @@ interface ILayoutProps {
 }
 
 export default function Layout({ children }: ILayoutProps) {
-    const router = useRouter()
-    console.log(router)
+    const { pathname } = useRouter()
+    const isHome = pathname === '/'
+    console.log(isHome)
     //is home
     return (
         <>
             <header className="py-3">
                 <div className="container mx-auto">
-                    <Link href="/" passHref>
-                        <a className="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clipRule="evenodd" />
-                            </svg>
-                            <span>Back</span>
-                        </a>
-                    </Link>
+                    {!isHome && (
+                        <Link href="/" passHref>
+                            <a className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clipRule="evenodd" />
+                                </svg>
+                                <span>Back</span>
+                            </a>
+                        </Link>
+                    )}
+
                 </div>
             </header>
             <div className="container py-16 lg:py-32 mx-auto">
--- pages/state-reducer.tsx
+++ pages/state-reducer.tsx
@@ -1,11 +1,112 @@
 import { NextPage } from "next";
+import * as React from "react";
 import Layout from "../components/Layout";
+import { Switch } from '@headlessui/react'
+
+interface ISwitchProps {
+
+}
+/* This example requires Tailwind CSS v2.0+ */
+import { useState } from 'react'
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
+
+
+
+const actionTypes = {
+    toggle: 'TOGGLE',
+    on: 'ON',
+    off: 'OFF'
+}
+interface IAction {
+    type: string
+}
+interface IState {
+    on: boolean
+}
+
+const ToggleReducer = (state: IState, action: IAction): IState => {
+    switch (action.type) {
+        case actionTypes.toggle:
+            return { on: !state.on }
+        case actionTypes.on: {
+            return { on: true }
+        }
+        case actionTypes.off: {
+            return { on: false }
+        }
+        default: {
+            throw new Error(`Unhandles action type: ${action.type}`)
+        }
+    }
+}
+function useToggle({ reducer = ToggleReducer } = {}) {
+    // const [on, setOnState] = React.useState<boolean>(false)
+    // const toggle = () => setOnState(o => !o)
+    // const setOn = () => setOnState(true)
+    // const setOff = () => setOnState(true)
+    const [{ on }, dispatch] = React.useReducer(reducer, { on: false })
+    const toggle = () => dispatch({ type: actionTypes.toggle })
+    const setOn = () => dispatch({ type: actionTypes.on })
+    const setOff = () => dispatch({ type: actionTypes.off })
+
+    return { on, toggle, setOn, setOff }
+}
+
 
 const StateReducerPage: NextPage = () => {
+
+    const [clicksSinceReset, setClicksSinceReset] = React.useState(0)
+    console.log(clicksSinceReset)
+    const tooMuchClick = clicksSinceReset >= 4
+    const { on, toggle, setOn, setOff } = useToggle({
+        reducer(currentState, action) {
+            const changes = ToggleReducer(currentState, action)
+            if (tooMuchClick && action.type === actionTypes.toggle) {
+                return { ...changes, on: currentState.on }
+            }
+            return changes
+        }
+    })
+    const [error, setError] = React.useState(false)
+    // console.log('on', on)
     return (
         <Layout>
             <div className="">
-                lets go
+
+                <div className="">
+                    <button onClick={setOn} disabled={tooMuchClick}>On</button>
+                    <button onClick={setOff} disabled={tooMuchClick}>Off</button>
+                    <Switch
+                        disabled={tooMuchClick}
+                        checked={on}
+
+                        onChange={() => { toggle(), setClicksSinceReset(n => n + 1) }}
+                        className={classNames(
+                            on ? 'bg-indigo-600' : 'bg-gray-200',
+                            'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                        )}
+                    >
+                        <span className="sr-only">Use setting</span>
+                        <span
+                            aria-hidden="true"
+                            className={classNames(
+                                on ? 'translate-x-5' : 'translate-x-0',
+                                'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+                            )}
+                        />
+                    </Switch>
+                    {tooMuchClick && (
+                        <div className="flex">
+                            <div className=" text-red-500">
+                                Error: You cannot switch more than 4 times
+                            </div>
+                            <button onClick={() => setClicksSinceReset(0)}>reset</button>
+                        </div>
+                    )}
+                </div>
             </div>
         </Layout>
 
